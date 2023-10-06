9d4f805181a802497365e78e733377d63b3fe708
Leonardo Garcia <leonardoggarcia48@gmail.com>
2023-10-06 08:22:54 -0300

    Add repository

--- startup.js
+++ startup.js
@@ -1,9 +1,13 @@
+const consign = require("consign")
 const express = require("express")
 
 const app = express()
 
 app.listen(3000, () => console.log("Online Server at port 3000"))
 
+consign()
+        .include("mvc/controllers")
+        .into(app)
+
 module.exports = app
 
-const itemsController = require("./mvc/controllers/itemsController.js")
